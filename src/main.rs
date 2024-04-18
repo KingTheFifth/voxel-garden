71541c11c38e2d3351b66da04ce4f096da248325
Gustav Sörnäs <gustav@sornas.net>
2024-04-18 13:37:19 +0200

    camera.rs

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,22 @@ use std::collections::HashMap;
 use std::mem::size_of;
 use std::sync::atomic;
 
-use glam::{IVec3, Mat3, Mat4, Quat, Vec3, Vec4};
+use glam::{IVec3, Mat4, Quat, Vec3, Vec4};
 use miniquad::{
     conf, date, window, Bindings, BufferLayout, BufferSource, BufferType, BufferUsage, Comparison,
     CullFace, EventHandler, KeyCode, PassAction, Pipeline, PipelineParams, RenderingBackend,
     ShaderSource, UniformsSource, VertexAttribute, VertexFormat, VertexStep,
 };
-use models::terrain::generate_terrain;
-use models::tree::tree;
 use noise::Perlin;
 use ringbuffer::{AllocRingBuffer, RingBuffer as _};
-use utils::arb_rotate;
+
+use crate::camera::{trackball_control, Movement};
+use crate::models::terrain::generate_terrain;
+use crate::models::tree::tree;
 
 static NEXT_ID: atomic::AtomicU64 = atomic::AtomicU64::new(0);
 
+mod camera;
 mod models;
 mod utils;
 
@@ -47,18 +49,6 @@ struct App {
     mouse_right_down: bool,
     mouse_prev_pos: (f32, f32),
     movement: Movement,
-}
-
-enum Movement {
-    Trackball {
-        down_pos: (f32, f32),
-        matrix: Mat4,
-    },
-    Flying {
-        position: Vec3,
-        look_h: f32,
-        look_v: f32,
-    },
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -336,33 +326,6 @@ impl App {
     }
 }
 
-fn trackball_camera_matrix() -> Mat4 {
-    let scale = 5.0;
-    Mat4::look_at_rh(
-        scale * Vec3::new(0.0, 0.0, 5.0),
-        scale * Vec3::ZERO,
-        Vec3::Y,
-    )
-}
-
-fn trackball_control(camera_matrix: Mat4, screen_pos: (f32, f32), prev_pos: (f32, f32)) -> Mat4 {
-    let axis = Vec3::new(screen_pos.1 - prev_pos.1, prev_pos.0 - screen_pos.0, 0.0);
-    let axis = Mat3::from_mat4(camera_matrix).inverse() * axis;
-    arb_rotate(axis, axis.length() / 50.0)
-}
-
-fn flying_camera_matrix(position: Vec3, angle_x: f32, angle_y: f32) -> Mat4 {
-    Mat4::look_at_rh(
-        position,
-        position
-            + (Mat4::from_quat(
-                (Quat::from_rotation_y(angle_y) * Quat::from_rotation_x(angle_x)).normalize(),
-            ) * Vec4::Z)
-                .truncate(),
-        Vec3::Y,
-    )
-}
-
 impl EventHandler for App {
     fn update(&mut self) {
         let now = date::now();
@@ -428,17 +391,7 @@ impl EventHandler for App {
 
         let projection =
             Mat4::perspective_rh_gl(self.fov_y_radians, self.aspect_ratio, 0.1, 1000.0);
-        let camera = match self.movement {
-            Movement::Trackball {
-                down_pos: _,
-                matrix: trackball_rotation_matrix,
-            } => trackball_camera_matrix() * trackball_rotation_matrix,
-            Movement::Flying {
-                position,
-                look_h,
-                look_v,
-            } => flying_camera_matrix(position, look_v, look_h),
-        };
+        let camera = self.movement.camera_matrix();
 
         self.ctx.apply_bindings(&self.cube.0);
         // self.draw_ground(projection, camera);
@@ -457,6 +410,7 @@ impl EventHandler for App {
         #[cfg(feature = "egui")]
         self.egui_mq.mouse_motion_event(x, y);
 
+        let camera_matrix = self.movement.camera_matrix();
         match &mut self.movement {
             Movement::Trackball {
                 down_pos: _,
@@ -464,8 +418,7 @@ impl EventHandler for App {
             } => {
                 if self.mouse_left_down {
                     *matrix =
-                        trackball_control(trackball_camera_matrix(), (x, y), self.mouse_prev_pos)
-                            * *matrix;
+                        trackball_control(camera_matrix, (x, y), self.mouse_prev_pos) * *matrix;
                 }
             }
             Movement::Flying {
