770862609cfde6292ba6ab6b15c3a798b95fc93e
Gustav Sörnäs <gustav@sornas.net>
2024-05-16 10:40:55 +0200

    remove old code

--- src/main.rs
+++ src/main.rs
@@ -378,31 +378,10 @@ impl App {
     }
 }
 
-fn trackball_camera_matrix() -> Mat4 {
-    let scale = 50.0;
-    Mat4::look_at_rh(
-        scale * Vec3::new(0.0, 0.0, 5.0),
-        scale * Vec3::ZERO,
-        Vec3::Y,
-    )
-}
-
 fn trackball_control(camera_matrix: Mat4, screen_pos: (f32, f32), prev_pos: (f32, f32)) -> Mat4 {
     let axis = Vec3::new(screen_pos.1 - prev_pos.1, prev_pos.0 - screen_pos.0, 0.0);
     let axis = Mat3::from_mat4(camera_matrix).inverse() * axis;
     arb_rotate(axis, axis.length() / 50.0)
-}
-
-fn flying_camera_matrix(position: Vec3, angle_x: f32, angle_y: f32) -> Mat4 {
-    Mat4::look_at_rh(
-        position,
-        position
-            + (Mat4::from_quat(
-                (Quat::from_rotation_y(angle_y) * Quat::from_rotation_x(angle_x)).normalize(),
-            ) * Vec4::Z)
-                .truncate(),
-        Vec3::Y,
-    )
 }
 
 impl EventHandler for App {
