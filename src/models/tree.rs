93f70db4c6c002192edfa24195a7c031434a8971
Johannes Kung <johku144@student.liu.se>
2024-05-23 16:43:59 +0200

    Supposedly more random trees

--- src/models/tree.rs
+++ src/models/tree.rs
@@ -1,4 +1,5 @@
 use glam::{Quat, Vec3};
+use rand::{thread_rng, Rng};
 
 use crate::models::primitives::{circle, sphere};
 use crate::models::Model;
@@ -24,8 +25,24 @@ pub fn tree(_seed: u64, translation: Vec3) -> Vec<Model> {
         translation,
     };
 
+    let mut rng = thread_rng();
+    let layers: i32 = 3;
+    let mut shrub = vec![];
+    for l in 0..layers {
+        for _n in 1..=6 - 2 * l {
+            shrub.extend(sphere(
+                Point::new(
+                    rng.gen_range(-(7 - 2 * l)..(7 - 2 * l)),
+                    tree_height + 5 * l,
+                    rng.gen_range(-(7 - 2 * l)..(7 - 2 * l)),
+                ),
+                15.0 - (2 * (l + 1)) as f32,
+            ));
+        }
+    }
     let shrub = Model {
-        points: sphere(Point::new(0, tree_height, 0), 15.0)
+        //points: sphere(Point::new(0, tree_height, 0), 15.0)
+        points: shrub
             .into_iter()
             .map(|Point { x, y, z }| InstanceData {
                 position: Vec3::new(x as f32, y as f32, z as f32),
