97041c1162a5212c7c56672736f980879f35154d
Gustav Sörnäs <gustav@sornas.net>
2024-05-24 16:55:01 +0200

    move more stuff to mod shader

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,20 @@
 use std::collections::HashSet;
-use std::mem::size_of;
 use std::sync::{mpsc, Arc, Mutex};
 use std::time::SystemTime;
 use std::{collections::HashMap, f32::consts::PI};
 
 use glam::{IVec2, IVec3, Mat4, Quat, Vec2, Vec3, Vec4};
 use miniquad::{
-    conf, date, window, Bindings, BufferLayout, BufferSource, BufferType, BufferUsage, Comparison,
-    CullFace, EventHandler, GlContext, KeyCode, PassAction, Pipeline, PipelineParams,
-    RenderingBackend, ShaderSource, TextureFormat, TextureKind, TextureParams, TextureWrap,
-    UniformsSource, VertexAttribute, VertexFormat, VertexStep,
+    conf, date, window, EventHandler, GlContext, KeyCode, PassAction, RenderingBackend as _,
 };
 use noise::Perlin;
-use rand::{thread_rng, Rng};
 use ringbuffer::{AllocRingBuffer, RingBuffer as _};
 
 use crate::camera::{trackball_control, Movement};
 use crate::models::biomes::BiomeConfig;
 use crate::models::terrain::{generate_terrain, GenerationPositions, TerrainConfig};
-use crate::shader::Uniforms;
+pub use crate::shader::InstanceData;
+use crate::shader::{Shader, Uniforms};
 
 mod camera;
 mod models;
@@ -28,13 +24,13 @@ mod utils;
 type Point = IVec3;
 type Terrain = HashMap<IVec2, GenerationPositions>;
 
-const MAX_INSTANCE_DATA: usize = size_of::<InstanceData>() * 100_000;
 const CHUNK_SIZE: i32 = 32;
 
 /// Contains state used by the application.
 struct App {
     /// The rendering context contains all state related to OpenGL managed by miniquad.
     ctx: GlContext,
+    shader: Shader,
     /// Current aspect ratio of the window. Used to calculate the perspective matrix.
     aspect_ratio: f32,
     /// Current target vertical FOV. Used to calculate the perspective matrix.
@@ -42,17 +38,6 @@ struct App {
     /// Contains state required for integrating the GUI library with miniquad.
     #[cfg(feature = "egui")]
     egui_mq: egui_miniquad::EguiMq,
-    /// A pipeline (rendering pipeline) collects information that is applied before draw
-    /// calls. It contains:
-    ///
-    /// - reference to compiled shader program
-    /// - vertex attribute configuration (stride, buffer index, etc)
-    /// - parameters such as culling, depth test, blends, ...
-    pipeline: Pipeline,
-    /// Tuple of bindings and amount of vertices.
-    ///
-    /// The bindings contain vertex buffer IDs, index buffer ID and any texture IDs.
-    cube: (Bindings, i32),
 
     /// The time at the previous call to update()
     prev_update: f64,
@@ -94,154 +79,12 @@ struct App {
     render_distance: i32,
 }
 
-/// Uploaded vertex data to the GPU
-#[repr(C)]
-struct VertexData {
-    position: Vec3,
-    normal: Vec3,
-}
-
-/// Uploaded instance data to the GPU
-#[repr(C)]
-#[derive(Clone, Copy)]
-struct InstanceData {
-    position: Vec3,
-    color: Vec4,
-    is_water: u32,
-}
-
-impl InstanceData {
-    fn new(position: Vec3, color: Vec4, is_water: u32) -> InstanceData {
-        InstanceData {
-            position,
-            color,
-            is_water,
-        }
-    }
-}
-
 impl App {
     fn new() -> Self {
         let mut ctx = GlContext::new();
         let (window_width, window_height) = window::screen_size();
-        // Compile the shader.
-        let shader = ctx
-            .new_shader(
-                ShaderSource::Glsl {
-                    vertex: shader::VERTEX,
-                    fragment: shader::FRAGMENT,
-                },
-                shader::meta(),
-            )
-            .unwrap();
 
-        let d = 0.5;
-        #[rustfmt::skip]
-        let vertices = [
-            VertexData { position: Vec3::new(-d, -d, -d), normal: Vec3::new( -d, 0.0, 0.0).normalize() },
-            VertexData { position: Vec3::new( d, -d, -d), normal: Vec3::new(  0.0, 0.0, -d).normalize() },
-            VertexData { position: Vec3::new(-d,  d, -d), normal: Vec3::new( -d,  d, -d).normalize() },
-            VertexData { position: Vec3::new( d,  d, -d), normal: Vec3::new(  d,  d, -d).normalize() },
-            VertexData { position: Vec3::new(-d, -d,  d), normal: Vec3::new( 0.0, -d,  0.0).normalize() },
-            VertexData { position: Vec3::new( d, -d,  d), normal: Vec3::new(  d, 0.0,  0.0).normalize() },
-            VertexData { position: Vec3::new(-d,  d,  d), normal: Vec3::new( 0.0,  d,  0.0).normalize() },
-            VertexData { position: Vec3::new( d,  d,  d), normal: Vec3::new(  0.0,  0.0,  d).normalize() },
-        ];
-
-        let geometry_vertex_buffer = ctx.new_buffer(
-            BufferType::VertexBuffer,
-            BufferUsage::Immutable,
-            BufferSource::slice(&vertices),
-        );
-
-        // Flat shading uses the attributes of the last vertex of a triangle
-        // for every fragment in it
-        // By making sure that both triangles for a side of a voxel shares the
-        // same last vertex, the entire side gets the same attributes such as
-        // surface normal
-        #[rustfmt::skip]
-        let indices = [
-            // Back
-            0, 2, 1,   2, 3, 1,
-            // Front
-            4, 5, 7,   6, 4, 7,
-            // Right
-            1, 3, 5,   3, 7, 5,
-            // Left
-            4, 6, 0,   6, 2, 0,
-            // Top
-            7, 3, 6,   3, 2, 6,
-            // Bottom
-            1, 5, 4,   0, 1, 4,
-
-        ];
-
-        let index_buffer = ctx.new_buffer(
-            BufferType::IndexBuffer,
-            BufferUsage::Immutable,
-            BufferSource::slice(&indices),
-        );
-
-        // Even though this says VertexBuffer, a bit further down we specify a buffer
-        // layout with `VertexStep::PerInstance`, meaning the data is the same for
-        // every vertex in its instance.
-        let instance_buffer = ctx.new_buffer(
-            BufferType::VertexBuffer,
-            BufferUsage::Stream, // TODO: dynamic?
-            BufferSource::empty::<InstanceData>(MAX_INSTANCE_DATA),
-        );
-
-        let mut random_bytes = [0u8; 1024 * 1024 * 4];
-        let mut rng = thread_rng();
-        for i in 0..(1024 * 1024) {
-            random_bytes[i * 4] = rng.gen();
-            random_bytes[i * 4 + 1] = rng.gen();
-            random_bytes[i * 4 + 2] = rng.gen();
-            random_bytes[i * 4 + 3] = 255;
-        }
-        let water_random_tex = ctx.new_texture_from_data_and_format(
-            &random_bytes,
-            TextureParams {
-                kind: TextureKind::Texture2D,
-                width: 1024,
-                height: 1024,
-                format: TextureFormat::RGBA8,
-                wrap: TextureWrap::Repeat,
-                ..Default::default()
-            },
-        );
-
-        let bindings = Bindings {
-            vertex_buffers: vec![geometry_vertex_buffer, instance_buffer],
-            index_buffer,
-            images: vec![water_random_tex],
-        };
-
-        let pipeline = ctx.new_pipeline(
-            &[
-                // buffer 0: geometry vertex buffer
-                BufferLayout::default(),
-                // buffer 1: instance "vertex" buffer
-                BufferLayout {
-                    step_func: VertexStep::PerInstance,
-                    ..BufferLayout::default()
-                },
-            ],
-            &[
-                VertexAttribute::with_buffer("in_position", VertexFormat::Float3, 0),
-                VertexAttribute::with_buffer("in_normal", VertexFormat::Float3, 0),
-                VertexAttribute::with_buffer("in_inst_position", VertexFormat::Float3, 1), // TODO: VertexFormat::Int32?
-                VertexAttribute::with_buffer("in_inst_color", VertexFormat::Float4, 1),
-                VertexAttribute::with_buffer("is_water", VertexFormat::Int1, 1),
-            ],
-            shader,
-            PipelineParams {
-                depth_test: Comparison::Less,
-                depth_write: true,
-                cull_face: CullFace::Back,
-                ..Default::default()
-            },
-        );
+        let shader = shader::Shader::new(&mut ctx);
 
         let terrain_config = TerrainConfig {
             sample_rate: 0.004,
@@ -278,10 +121,11 @@ impl App {
         let mut app = Self {
             #[cfg(feature = "egui")]
             egui_mq: egui_miniquad::EguiMq::new(&mut ctx),
+
             ctx,
+            shader,
             aspect_ratio: 1.0,
             fov_y_radians: 1.0,
-            pipeline,
             prev_update: 0.0,
             prev_draw: 0.0,
             fps_history: AllocRingBuffer::new(100),
@@ -289,7 +133,6 @@ impl App {
             terrain_config,
             terrain_chunk_gen_queue: terrain_chunk_gen_queue.0,
             terrain_chunk_waiting: HashSet::new(),
-            cube: (bindings, indices.len() as i32),
             sun_direction: Vec3::new(1.0, 1.0, 0.0),
             sun_color: Vec4::new(1.0, 1.0, 0.2, 1.0),
             ambient_light_color: Vec4::new(0.7, 0.7, 0.7, 1.0),
@@ -510,13 +353,17 @@ impl App {
         )
     }
 
-    fn draw_chunk(
+    fn draw_chunk_and_around(
         &mut self,
         projection: Mat4,
         camera: Mat4,
         camera_position: IVec2,
         camera_look_h: Option<f32>,
     ) {
+        let time = SystemTime::now()
+            .duration_since(self.system_time)
+            .unwrap()
+            .as_secs_f32();
         // Which chunk is the camera located in?
         let camera_chunk = IVec2::new(
             camera_position.x / CHUNK_SIZE,
@@ -550,68 +397,24 @@ impl App {
                 }
                 let chunk_data = terrain.get(&chunk).unwrap();
 
-                // TODO: remove
-                let spawn_point_instance_data: Vec<_> = chunk_data
-                    .spawn_points
-                    .iter()
-                    .map(|sp| sp.instance_data)
-                    .collect();
-
                 // Draw ground
-                self.ctx.buffer_update(
-                    self.cube.0.vertex_buffers[1],
-                    BufferSource::slice(&chunk_data.ground),
-                );
-                self.ctx.apply_uniforms(UniformsSource::table(
-                    &self.uniforms(
-                        projection,
-                        camera,
-                        camera,
-                        SystemTime::now()
-                            .duration_since(self.system_time)
-                            .unwrap()
-                            .as_secs_f32(),
-                    ),
-                ));
-                self.ctx
-                    .draw(0, self.cube.1, chunk_data.ground.len() as i32);
-
-                // TODO: remove
-                // draw spawn points
-                // dont need to apply uniforms since spawn points
-                // can be treated as ground voxels
-                self.ctx.buffer_update(
-                    self.cube.0.vertex_buffers[1],
-                    BufferSource::slice(&spawn_point_instance_data),
-                );
-                self.ctx
-                    .draw(0, self.cube.1, chunk_data.spawn_points.len() as i32);
+                let uniforms = self.uniforms(projection, camera, camera, time);
+                self.shader
+                    .draw_voxels(&mut self.ctx, &chunk_data.ground, &uniforms);
 
                 // First collect all models (in the current chunk) in an iterator
                 let models = chunk_data.objects.iter().flatten();
 
                 // Then draw each model one at a time
                 for model in models {
-                    self.ctx.buffer_update(
-                        self.cube.0.vertex_buffers[1],
-                        BufferSource::slice(&model.points),
+                    let uniforms = self.uniforms(
+                        projection,
+                        camera * Mat4::from_rotation_translation(model.rotation, model.translation),
+                        camera,
+                        time,
                     );
-                    self.ctx.apply_uniforms(UniformsSource::table(
-                        &self.uniforms(
-                            projection,
-                            camera
-                                * Mat4::from_rotation_translation(
-                                    model.rotation,
-                                    model.translation,
-                                ),
-                            camera,
-                            SystemTime::now()
-                                .duration_since(self.system_time)
-                                .unwrap()
-                                .as_secs_f32(),
-                        ),
-                    ));
-                    self.ctx.draw(0, self.cube.1, model.points.len() as i32);
+                    self.shader
+                        .draw_voxels(&mut self.ctx, &model.points, &uniforms);
                 }
             }
         }
@@ -734,6 +537,8 @@ impl EventHandler for App {
         self.prev_draw = now;
         self.fps_history.push(1.0 / draw_delta);
 
+        self.shader.prepare_draw(&mut self.ctx);
+
         // sky
         self.ctx.begin_default_pass(PassAction::clear_color(
             0x87 as f32 / 255.0,
@@ -742,12 +547,9 @@ impl EventHandler for App {
             1.0,
         ));
 
-        // Beware the pipeline
-        self.ctx.apply_pipeline(&self.pipeline);
-
         let projection =
             Mat4::perspective_rh_gl(self.fov_y_radians, self.aspect_ratio, 0.1, 1000.0);
-        // FIXME: uh oh, sthinky
+        // FIXME
         let camera = self.movement.camera_matrix()
             * match self.movement {
                 Movement::Trackball { matrix, .. } => matrix,
@@ -765,9 +567,7 @@ impl EventHandler for App {
             Movement::Flying { look_h, .. } | Movement::OnGround { look_h, .. } => Some(look_h),
         };
 
-        self.ctx.apply_bindings(&self.cube.0);
-        self.draw_chunk(projection, camera, camera_position_2d, camera_look_h);
-        //self.draw_voxels(projection, camera); Use this when?
+        self.draw_chunk_and_around(projection, camera, camera_position_2d, camera_look_h);
 
         self.ctx.end_render_pass();
 
